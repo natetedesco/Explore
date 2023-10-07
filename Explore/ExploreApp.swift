2c576d2f51bea9774ebff045e23b631eba1bb137
Nate Tedesco <Natetedesco@icloud.com>
2023-10-07 15:08:48 -0700

    Initial Commit

---
+++ Explore/ExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreApp.swift
+//  Explore
+//
+//  Created by Nate Tedesco on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
